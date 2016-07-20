8de194dc2461503ce4c0bdf2ce028239f4b1f9c8
Mark Caruso <kazoo6723@gmail.com>
2016-07-20 19:40:56 -0400

    Deleted routes/animals.js and routes cars.js. Modified crew.ejs and index.js to handle additional routes in index.js

--- routes/index.js
+++ routes/index.js
@@ -5,16 +5,48 @@ var appdata = require('../data.json');
 /* Get Home Page */
 router.get('/', function(req, res){
 	var myPhotos = [];
+	var myCrew = [];
+	myCrew = appdata.crew;
 	appdata.crew.forEach(function(item){
 		myPhotos = myPhotos.concat(item.photos);
 	})
 	res.render('index', {
 		title: 'Home',
+		photos: myPhotos,
+		crew: myCrew;
+	});
+});
+
+/* Get the crew template */
+router.get('/crew', function(req, res){
+	var myPhotos = [];
+	var myCrew = [];
+	myCrew = appdata.crew;
+	appdata.crew.forEach(function(item){
+		myPhotos = myPhotos.concat(item.photos);
+	});
+	res.render('crew', {
+		title: 'Crew',
+		photos: myPhotos,
+		crew: myCrew
+	});
+});
+
+router.get('/crew/:crewId', function(req, res){
+	var myPhotos = [];
+	appdata.crew.forEach(function(item){
+
+		if(item.shortname == req.params.crewId){
+		myPhotos = myPhotos.concat(item.photos);			
+		}
+
+	});
+	res.render('crew', {
+		title: 'Crew',
 		photos: myPhotos
 
 	});
 });
-
 
 
 module.exports = router; 
